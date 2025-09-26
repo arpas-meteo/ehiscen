627224d3b905e712f3340681158b569ec766b8bb
Andrea Lai <alai.arpas@gmail.com>
2025-09-26 14:26:44 +0200

    documenta csv oracle

--- lib/ehiscen/his_csv_from_oracle.ex
+++ lib/ehiscen/his_csv_from_oracle.ex
@@ -1,12 +1,21 @@
 defmodule Ehiscen.HisCsvfromOracle do
+  @moduledoc """
+  Documentation for `Ehiscen.HisCsvfromOracle`.
+
+  Modulo specializzato nella lettura dei file csv esportati da Oracle
+
+  Sono file senza intestazione
+
+  Vedi la funzione `rinomina_colonne_map` per vedere le colonne
+
+  """
+
   require Explorer.DataFrame, as: DF
   # require Explorer.Series, as: DS
 
   def grandezze, do: ~w(cod_staz)
 
-  def colonne do
-    ~w(cod_staz cod_grand data_mis valore cod_valid liv_validaz rete)
-  end
+  def colonne, do: ~w(cod_staz cod_grand data_mis valore cod_valid liv_validaz rete)
 
   def dtypes do
     # in csv 2020 sono presenti delle "A" come liv_validaz (column_6)
@@ -22,6 +31,24 @@ defmodule Ehiscen.HisCsvfromOracle do
       {"column_7", :string}
     ]
   end
+
+  @doc """
+  RinominaColonneMap
+
+  ## Examples
+
+      iex> Ehiscen.HisCsvfromOracle.rinomina_colonne_map()
+      %{
+      "column_1" => "cod_staz",
+      "column_2" => "cod_grand",
+      "column_3" => "data_ita",
+      "column_4" => "valore",
+      "column_5" => "cod_valid",
+      "column_6" => "liv_validaz",
+      "column_7" => "rete"
+    }
+
+  """
 
   def rinomina_colonne_map do
     %{
